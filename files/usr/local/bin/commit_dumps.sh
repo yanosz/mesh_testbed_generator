aae4ee9317c9773a22b38dde2c74df5251960a60
kaleng <kaleng@gokk.de>
2017-07-03 12:32:44 +0200

    fix commit_dumps.sh script

--- files/usr/local/bin/commit_dumps.sh
+++ files/usr/local/bin/commit_dumps.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-commit_interval=600 # Commit every 300 seconds aka 10 mins
+commit_interval=600 # Commit every 600 seconds aka 10 mins
 mkdir -p /usr/dumps
 mkdir -p /tmp/old_dumps
 hostname=$(uci get system.@system[0].hostname)
-date=$(date +%s)
 while (true) do
-
+  date=$(date +%s)
   #Create Archive
   mv /tmp/dumps/* /tmp/old_dumps
   tar czf /tmp/dumps.tar.gz /tmp/old_dumps
